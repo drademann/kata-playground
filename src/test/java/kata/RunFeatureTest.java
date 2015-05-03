4e8af97c0437a3ec20b59897afad5743d7d2a642
Dirk Rademann <contact@beyondworlds.net>
2015-05-03 15:36:33 +0200

    Switched to JUnit.

--- src/test/java/kata/RunFeatureTest.java
+++ src/test/java/kata/RunFeatureTest.java
@@ -1,10 +1,9 @@
 package kata;
 
-import cucumber.api.CucumberOptions;
-import cucumber.api.testng.AbstractTestNGCucumberTests;
+import cucumber.api.junit.*;
+import org.junit.runner.*;
 
-@CucumberOptions(
-		features = "src/test/resources/kata/stringcalc/",
-		glue = "kata.stringcalc")
-public class RunFeatureTest extends AbstractTestNGCucumberTests {
+@RunWith(Cucumber.class)
+public class RunFeatureTest {
+	// to let JUnit run the feature tests outside of an IDE
 }
--- src/test/java/kata/stringcalc/StringCalculatorStepDef.java
+++ src/test/java/kata/stringcalc/StringCalculatorStepDef.java
@@ -1,8 +1,9 @@
 package kata.stringcalc;
 
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+import cucumber.api.java.en.*;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
 
 public class StringCalculatorStepDef {
 
@@ -22,6 +23,6 @@ public class StringCalculatorStepDef {
 
 	@Then("^the result should be (\\d+)$")
 	public void the_result_should_be(int expectedResult) throws Throwable {
-		assert result == expectedResult : "expected result is " + expectedResult + " but was " + result;
+		assertThat(result, equalTo(expectedResult));
 	}
 }
