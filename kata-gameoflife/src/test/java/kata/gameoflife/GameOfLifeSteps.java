f94ca17c940685f4bd6a0d01ae253e6f08fab827
Dirk Rademann <contact@beyondworlds.net>
2015-05-17 18:12:16 +0200

    Removed predefined steps from Game of Life Kata.

--- kata-gameoflife/src/test/java/kata/gameoflife/GameOfLifeSteps.java
+++ kata-gameoflife/src/test/java/kata/gameoflife/GameOfLifeSteps.java
@@ -1,6 +1,5 @@
 package kata.gameoflife;
 
-import cucumber.api.java.en.*;
 import kata.gameoflife.support.*;
 
 public class GameOfLifeSteps {
@@ -9,20 +8,5 @@ public class GameOfLifeSteps {
 
 	public GameOfLifeSteps(KnowsGameOfLifeDomain gameOfLifeDomain) {
 		this.gameOfLifeDomain = gameOfLifeDomain;
-	}
-
-	@Given("^a live cell$")
-	public void givenLiveCell() {
-		// TODO implement or write some other step
-	}
-
-	@When("^having (\\d+) life neighbour\\(s\\)$")
-	public void whenHavingLifeNeighbours(int numberOfLiveNeighbours) throws Throwable {
-		// TODO implement or write some other step
-	}
-
-	@Then("^the cell dies$")
-	public void thenCellDies() {
-		// TODO implement or write some other step
 	}
 }
--- kata-gameoflife/src/test/resources/kata/game-of-life.feature
+++ kata-gameoflife/src/test/resources/kata/game-of-life.feature
@@ -7,7 +7,4 @@ Feature: Conways Game of Life
   3. Any live cell with more than three live neighbours dies, as if by overcrowding.
   4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
 
-  Scenario: Any live cell with fewer than two live neighbours dies, as if caused by under-population.
-    Given a live cell
-    When having 1 life neighbour(s)
-    Then the cell dies
+
