c9570ea3b04191e63e5a9a38d42f9aca1084e151
Dirk Rademann <contact@beyondworlds.net>
2015-10-04 10:53:47 +0200

    Switched to AssertJ, cleanup.

--- src/test/java/kata/PrimeFactorsTest.java
+++ src/test/java/kata/PrimeFactorsTest.java
@@ -1,10 +1,11 @@
 package kata;
 
-import org.fest.assertions.api.Assertions;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * <strong>Requirements</strong>
@@ -16,10 +17,21 @@ public class PrimeFactorsTest {
 
 	@Test
 	public void generate_shouldReturnPrimeFactors() {
-		Assertions.assertThat(generatePrimeFactors(1)).isEmpty();
+		assertThat(generatePrimeFactors(1)).isEmpty();
+		assertThat(generatePrimeFactors(2)).containsExactly(2);
+		assertThat(generatePrimeFactors(3)).containsExactly(3);
+		assertThat(generatePrimeFactors(4)).containsExactly(2, 2);
+		assertThat(generatePrimeFactors(5)).containsExactly(5);
+		assertThat(generatePrimeFactors(6)).containsExactly(2, 3);
+		assertThat(generatePrimeFactors(8)).containsExactly(2, 2, 2);
+		assertThat(generatePrimeFactors(9)).containsExactly(3, 3);
 	}
 
 	public static List<Integer> generatePrimeFactors(int n) {
-		return new ArrayList<>();
+		List<Integer> primes = new ArrayList<>();
+		for (int candidate = 2; n > 1; candidate++)
+			for (; n % candidate == 0; n /= candidate)
+				primes.add(candidate);
+		return primes;
 	}
 }
