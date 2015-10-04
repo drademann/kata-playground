6332cdd1303d3a97791ad3b1664720df5f635ad1
Dirk Rademann <contact@beyondworlds.net>
2015-10-04 23:15:35 +0200

    Example for String Calculator.

--- src/test/java/kata/StringCalculatorTest.java
+++ src/test/java/kata/StringCalculatorTest.java
@@ -2,7 +2,15 @@ package kata;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.unmodifiableList;
+import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
 
 /**
  * <strong>Requirements</strong>
@@ -44,16 +52,128 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class StringCalculatorTest {
 
 	@Test
-	public void givenEmptyStringReturnsZero() {
-		int result = new StringCalculator().calc("");
-
-		assertThat(result).isEqualTo(0);
+	public void emptyString_shouldReturnsZero() {
+		assertThat(calculatedSum("")).isEqualTo(0);
 	}
 
-	private static class StringCalculator {
+	@Test
+	public void oneNumber_shouldReturnNumberAsInteger() {
+		assertThat(calculatedSum("1")).isEqualTo(1);
+	}
 
-		public int calc(String s) {
+	@Test
+	public void twoCommaSeparatedNumbers_shouldReturnSumAsInteger() {
+		assertThat(calculatedSum("1,2")).isEqualTo(3);
+	}
+
+	@Test
+	public void multipleCommaSeparatedNumbers_shouldReturnSumAsInteger() {
+		assertThat(calculatedSum("1,2,5,20")).isEqualTo(28);
+	}
+
+	@Test
+	public void lineBreakAsDelimiter_shouldBeAcceptedLikeComma() {
+		assertThat(calculatedSum("1\n2,3")).isEqualTo(6);
+	}
+
+	@Test
+	public void selfDefinedDelimiter_shouldBeAcceptedLikeComma() {
+		assertThat(calculatedSum("//;\n1;2\n3")).isEqualTo(6);
+	}
+
+	@Test(expected = NegativeNumbersException.class)
+	public void givenNegativNumbers_shouldThrowException() {
+		calculatedSum("-1,2,-3");
+	}
+
+	@Test
+	public void givenNegativeNumbers_shouldThrowExceptionContainingNegativeIntegers() {
+		try {
+			calculatedSum("-1,2, -3");
+			fail("should throw exception");
+		} catch (NegativeNumbersException nne) {
+			assertThat(nne.getNegativeNumbers()).contains(-1, -3);
+		}
+	}
+
+	private int calculatedSum(String s) {
+		return new StringCalculator(s).sum();
+	}
+
+	public static class StringCalculator {
+
+		private final String input;
+
+		private String delimiter;
+		private Collection<Integer> integers;
+
+		public StringCalculator(String input) {
+			this.input = input;
+		}
+
+		public int sum() {
+			if (isParsable()) {
+				delimiter = lookupDelimiter();
+				integers = collectIntegers();
+				if (containsOnlyPositive())
+					return sumIntegers();
+			}
 			return 0;
+		}
+
+		private boolean isParsable() {
+			return !input.isEmpty();
+		}
+
+		private String lookupDelimiter() {
+			String userDefinedDelimiter = "";
+			if (input.startsWith("//")) {
+				userDefinedDelimiter = input.substring(2, 3);
+			}
+			return "[,\n" + userDefinedDelimiter + "]";
+		}
+
+		private Collection<Integer> collectIntegers() {
+			return splittedInput().stream()
+					.map(String::trim)
+					.filter(this::isNumber)
+					.mapToInt(Integer::parseInt)
+					.boxed()
+					.collect(toList());
+		}
+
+		private boolean containsOnlyPositive() {
+			List<Integer> negatives = integers.stream().filter(i -> i < 0).collect(toList());
+			if (!negatives.isEmpty()) {
+				throw new NegativeNumbersException(negatives);
+			}
+			return true;
+		}
+
+		private int sumIntegers() {
+			return integers.stream().mapToInt(Integer::intValue).sum();
+		}
+
+		private Collection<String> splittedInput() {
+			return asList(input.split(delimiter));
+		}
+
+		private boolean isNumber(String s) {
+			return s != null && !s.isEmpty() && !s.startsWith("//");
+		}
+	}
+
+	private static final class NegativeNumbersException extends RuntimeException {
+		private static final long serialVersionUID = -1L;
+
+		private final List<Integer> negativeNumbers;
+
+		private NegativeNumbersException(List<Integer> negativeNumbers) {
+			this.negativeNumbers = new ArrayList<>(negativeNumbers);
+		}
+
+		public List<Integer> getNegativeNumbers() {
+			return unmodifiableList(negativeNumbers);
 		}
 	}
 }
