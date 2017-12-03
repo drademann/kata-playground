e019ca2e1789be457e67e10b89fd0e57d637cadd
Dirk Rademann <dirk.rademann@icloud.com>
2017-12-03 21:40:30 +0100

    Reformatting PrimeFactors.

--- src/main/java/kata/primefactor/PrimeFactors.java
+++ src/main/java/kata/primefactor/PrimeFactors.java
@@ -5,12 +5,13 @@ import java.util.List;
 
 class PrimeFactors {
 
-	static List<Integer> of(int n) {
-		List<Integer> primes = new ArrayList<>();
-		for (int candidate = 2; n > 1; candidate++)
-			for (; n % candidate == 0; n /= candidate)
-				primes.add(candidate);
-		return primes;
-	}
+    static List<Integer> of(int number) {
+        int n = number;
+        List<Integer> primes = new ArrayList<>();
+        for (int candidate = 2; n > 1; candidate++)
+            for (; (n % candidate) == 0; n /= candidate)
+                primes.add(candidate);
+        return primes;
+    }
 
 }
--- src/test/java/kata/primefactor/PrimeFactorsTest.java
+++ src/test/java/kata/primefactor/PrimeFactorsTest.java
@@ -12,16 +12,16 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 class PrimeFactorsTest {
 
-	@Test
-	void generate_shouldReturnPrimeFactors() {
-		assertThat(PrimeFactors.of(1)).isEmpty();
-		assertThat(PrimeFactors.of(2)).containsExactly(2);
-		assertThat(PrimeFactors.of(3)).containsExactly(3);
-		assertThat(PrimeFactors.of(4)).containsExactly(2, 2);
-		assertThat(PrimeFactors.of(5)).containsExactly(5);
-		assertThat(PrimeFactors.of(6)).containsExactly(2, 3);
-		assertThat(PrimeFactors.of(8)).containsExactly(2, 2, 2);
-		assertThat(PrimeFactors.of(9)).containsExactly(3, 3);
-	}
+    @Test
+    void generate_shouldReturnPrimeFactors() {
+        assertThat(PrimeFactors.of(1)).isEmpty();
+        assertThat(PrimeFactors.of(2)).containsExactly(2);
+        assertThat(PrimeFactors.of(3)).containsExactly(3);
+        assertThat(PrimeFactors.of(4)).containsExactly(2, 2);
+        assertThat(PrimeFactors.of(5)).containsExactly(5);
+        assertThat(PrimeFactors.of(6)).containsExactly(2, 3);
+        assertThat(PrimeFactors.of(8)).containsExactly(2, 2, 2);
+        assertThat(PrimeFactors.of(9)).containsExactly(3, 3);
+    }
 
 }
