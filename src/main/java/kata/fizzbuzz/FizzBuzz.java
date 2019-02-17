6fa1a89621e61086839fc714ae088f0fb5830c3c
Dirk Rademann <dirk.rademann@icloud.com>
2019-02-17 09:16:33 +0100

    Implements a FizzBuzz version.

--- src/main/java/kata/fizzbuzz/FizzBuzz.java
+++ src/main/java/kata/fizzbuzz/FizzBuzz.java
@@ -1,8 +1,43 @@
 package kata.fizzbuzz;
 
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
+
 final class FizzBuzz {
 
     public static String[] map(int... numbers) {
-        return new String[]{"1", "2"};
+        return new FizzBuzz(IntStream.of(numbers)).mapInts();
     }
+
+    //<editor-fold desc="implementation">
+    private final IntStream intStream;
+
+    private FizzBuzz(IntStream intStream) {
+        this.intStream = intStream;
+    }
+
+    private String[] mapInts() {
+        return intStream.mapToObj(this::mapToString)
+                        .collect(toList())
+                        .toArray(String[]::new);
+    }
+
+    private String mapToString(int number) {
+        if (isFizz(number))
+            return "Fizz";
+        else if (isBuzz(number))
+            return "Buzz";
+        else
+            return Integer.toString(number);
+    }
+
+    private boolean isBuzz(int number) {
+        return number % 5 == 0;
+    }
+
+    private boolean isFizz(int number) {
+        return number % 3 == 0;
+    }
+    //</editor-fold>
 }
--- src/test/java/kata/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/kata/fizzbuzz/FizzBuzzTest.java
@@ -2,14 +2,36 @@ package kata.fizzbuzz;
 
 import org.junit.Test;
 
+import static kata.fizzbuzz.FizzBuzz.map;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FizzBuzzTest {
 
     @Test
     public void given_1_2_shouldReturn_1_2() {
-        var mapped = FizzBuzz.map(1, 2);
+        var mapped = map(1, 2);
 
         assertThat(mapped).containsExactly("1", "2");
+    }
+
+    @Test
+    public void given_3_shouldReturn_Fizz() {
+        var mapped = map(3);
+
+        assertThat(mapped).containsExactly("Fizz");
+    }
+
+    @Test
+    public void given_5_shouldReturn_Buzz() {
+        var mapped = map(5);
+
+        assertThat(mapped).containsExactly("Buzz");
+    }
+
+    @Test
+    public void given_ListOfNumbers_shouldMapAccordingly() {
+        var mapped = map(1, 2, 3, 4, 5, 6);
+
+        assertThat(mapped).containsExactly("1", "2", "Fizz", "4", "Buzz", "Fizz");
     }
 }
